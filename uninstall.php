f37fddbdf9b32d2f0db3a60305adae21fb447c34
gplatty <gplatty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-20 20:06:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitter-feeder/trunk@521482 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,1 @@
+<?php/*	The uninstall file	(to make your blog always clean when you need to test plugins :p)*/global $wpdb;if( !defined( 'ABSPATH') &&  !defined('WP_UNINSTALL_PLUGIN') )	    exit();		delete_option( 'widget_twitter_wp_widget');
